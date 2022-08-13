a8e7c08b2d14ef53483595f152c23b6a7f11724a
Derrick Fleming <derrick.fleming7@gmail.com>
2022-08-13 21:18:33 +0000

    Completed meme gallery project

---
+++ main.js
@@ -0,0 +1,24 @@
+const $gallery = document.querySelector('main');
+const imageUrls = [];
+const $imageUrlInput = document.querySelector('input');
+const $addImageButton = document.querySelector('button');
+
+$addImageButton.addEventListener('click', function () {
+  if ($imageUrlInput.value !== '') {
+    imageUrls.push($imageUrlInput.value);
+    $imageUrlInput.value = '';
+    updateGallery();
+  }
+});
+
+function updateGallery() {
+  $gallery.innerHTML = '';
+  for (let i = 0; i < imageUrls.length; i++) {
+    const $imageElement = document.createElement('img');
+    $imageElement.className = 'gallery-image';
+    $imageElement.src = imageUrls[i];
+    $gallery.appendChild($imageElement);
+  }
+}
+
+updateGallery();
